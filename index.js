02841b2a3186c21d9b779a9d3d8235ac62ff201e
Florencia Re <florencia.re@hotmail.com>
2023-11-05 11:37:26 -0300

    funcion comprar tickets terminada

---
+++ index.js
@@ -0,0 +1,78 @@
+// Defino valor de ticket
+const valorTicket = 200;
+
+// Defino porcentajes de descuento segÃºn categorÃ­a
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
+
+// Elementos en variables
+let nombre          = document.getElementById("nombre");
+let apellido        = document.getElementById("apellido");
+let mail            = document.getElementById("mail");
+let cantidadTickets = document.getElementById("cantidadTickets");
+let categoria       = document.getElementById("categoriaSelect");
+
+// FunciÃ³n para quitar el estilo de error a los elementos del form
+function quitarClaseError() {
+    let x = document.querySelectorAll(".form-control, .form-select");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('is-invalid');
+    }
+}
+
+// CÃ¡lculo total a pagar
+function total_a_pagar() {
+
+    // Ejecuto función para que quite todos los estilos de error en los campos que los tuvieran
+    quitarClaseError();
+
+    // Verifico si está ingresado al menos 1 ticket, sino que aplique un estilo de error, haga foco en el campo y se detenga
+   
+    if ( (cantidadTickets.value == 0) || (isNaN(cantidadTickets.value)) ) {
+        alert("Por favor, ingrese correctamente cantidad de tickets.");
+        cantidadTickets.classList.add("is-invalid");
+        cantidadTickets.focus();
+        return;
+    }
+
+    // Verifico que haya seleccionado una categorÃ­a, sino que aplique un estilo de error, haga foco en el campo y se detenga
+    if (categoria.value == "") {
+        alert("Por favor, seleccione una categoría.");
+        categoria.classList.add("is-invalid");
+        categoria.focus();
+        return;
+    }
+
+    // Multiplico cantidad de tickets por el valor
+    let totalValorTickets = (cantidadTickets.value) * valorTicket;
+
+    // Aplico descuentos segÃºn categorÃ­a
+    if (categoria.value == 0) {
+        totalValorTickets = totalValorTickets ;
+    }
+    if (categoria.value == 1) {
+        totalValorTickets = totalValorTickets - ( (descuentoEstudiante / 100) * totalValorTickets);
+    }
+    if (categoria.value == 2) {
+        totalValorTickets = totalValorTickets - (descuentoTrainee / 100 * totalValorTickets);
+    }
+    if (categoria.value == 3) {
+        totalValorTickets = totalValorTickets - (descuentoJunior / 100 * totalValorTickets);
+    }
+
+    // Inserto el valor en el HTML
+    totalPago.innerHTML = totalValorTickets;
+}
+
+// Boton Resumen recibe un escuchador y la funcion del cÃ¡lculo
+btnResumen.addEventListener('click', total_a_pagar);
+
+// Funcion para el boton Borrar para que borre el valor
+function reset_total_a_pagar() {
+    quitarClaseError();
+    totalPago.innerHTML = "";
+}
+
+btnBorrar.addEventListener('click', reset_total_a_pagar);
